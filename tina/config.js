c704d2f99e9bf0f6e1411c4a3485f9906fcecfcc
Revenciel <clarkybox@gmail.com>
2025-01-22 15:32:48 -0500

    update branch in config

--- tina/config.js
+++ tina/config.js
@@ -6,10 +6,10 @@ import global from "./collections/site-settings";
 
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD, // Netlify branch env
+  branch: "main",
+    // process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+    // process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+    // process.env.HEAD, // Netlify branch env
   token: process.env.TINA_TOKEN,
   media: {
     // If you wanted cloudinary do this
